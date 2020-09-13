195d2232cfc4e6340b36b0c883677aaf50eb8ceb
Nouni EL Bachir <nouni.elbachir@gmail.com>
2020-09-13 16:29:39 +0000

    use default constructor on the specified one

--- assignment1/Identifier.java
+++ assignment1/Identifier.java
@@ -6,24 +6,24 @@ public class Identifier implements IdentifierInterface {
 
     private StringBuffer identifier;
 
-    Identifier(){
+    Identifier() {
         identifier = new StringBuffer();
     }
 
-    Identifier(Identifier src){
-        identifier = new StringBuffer();
+    Identifier(Identifier src) {
+        this();
         identifier.append(src.identifier);
     }
 
     @Override
     public void init(char c) {
-        identifier.delete(0,identifier.length());
+        identifier.delete(0, identifier.length());
         identifier.append(c);
     }
 
     @Override
-    public void add(char c){
-         identifier.append(c);
+    public void add(char c) {
+        identifier.append(c);
     }
 
     @Override
